5a4f2e0e974d1f2ab5b91481ce9b543507cca5fa
ManiyaJain <maniyajain024@gmail.com>
2025-07-30 17:10:54 +0530

    Professor and Parties

---
+++ Professor and Parties.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public static String PartyType(int arr[]) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int val : arr){
+            if(map.get(val) == null){
+                map.put(val, true);
+            }
+            else{
+                return "true";
+            }
+        }
+        return "false";
+    }
+}
